34b3113fe2a3962a127a4dde9fdf3d3dfcda8c64
Paulo Freitas <paulo.freitas@c2alocacoes.dmn>
2021-11-19 12:22:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao')
+
+conexao.connect( (error)=> {
+    if(error){
+        console.log(error);
+    } else 
+        console.log("Conectado com sucesso");
+                
+        const app = customExpress();
+
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'));
+
+})
+
